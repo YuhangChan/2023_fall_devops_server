7b80308ccac7ac312c220449bf2dac21dcf9c142
Loganloxsins <211870018@smail.nju.edu.cn>
2023-11-12 20:58:37 +0800

    feat:sast

---
+++ src/main/java/org/SASTSample.java
@@ -0,0 +1,12 @@
+package org.fffd.l23o6;
+
+public class SATASample {
+    public boolean VerifyAdmin(String password) {
+        if (password.equals("68af404b513073584c4b6f22b6c63e6b")) {
+            System.out.println("Entering Diagnostic Mode...");
+            return true;
+        }
+        System.out.println("Incorrect Password!");
+        return false;
+    }
+}
